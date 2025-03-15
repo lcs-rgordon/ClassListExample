9a9bb0a40890e56e9b178b60b6eb60c7e6cd2026
Russell Gordon <rgordon@lcs.on.ca>
2025-03-15 13:00:15 -0400

    Made it possible to delete a student, but, right now sometimes the wrong student is deleted.

--- ClassListExample/Views/ClassListView.swift
+++ ClassListExample/Views/ClassListView.swift
@@ -17,9 +17,34 @@ struct ClassListView: View {
         NavigationStack {
             List(students, id: \.self) { currentStudent in
                 Text(currentStudent)
+                    .swipeActions {
+                        Button("Delete") {
+                            withAnimation {
+                                delete(providedStudent: currentStudent)
+                            }
+                        }
+                        .tint(.red)
+                    }
             }
             .navigationTitle("Class List")
         }
+    }
+    
+    // MARK: Function(s)
+    func delete(providedStudent: String) {
+        
+        // Iterate over the elements of the array and find the student to delete
+        for i in 0...students.count - 1 {
+            if students[i] == providedStudent {
+                
+                // Remove the current student
+                students.remove(at: i)
+                
+                // Exit the loop (don't delete any more students)
+                break
+            }
+        }
+        
     }
 
 }
