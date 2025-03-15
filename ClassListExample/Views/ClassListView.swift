7e967af633b4d69f4c355b84173727a6579e4b43
Russell Gordon <rgordon@lcs.on.ca>
2025-03-15 13:55:42 -0400

    The correct student is now deleted from the list when student names are the same.

--- ClassListExample/Views/ClassListView.swift
+++ ClassListExample/Views/ClassListView.swift
@@ -7,16 +7,34 @@
 
 import SwiftUI
 
+// MODEL
+struct Student: Identifiable {
+    
+    // MARK: Stored properties
+    let id: Int
+    let name: String
+    
+}
+
+// VIEW
 struct ClassListView: View {
     
     // MARK: Stored properties
-    @State var students: [String] = ["Alice", "Bob", "Charlie", "Daniela", "Bob"]
+    @State var students: [Student] = [
+        
+        Student(id: 1, name: "Alice"),
+        Student(id: 2, name: "Bob"),
+        Student(id: 3, name: "Charlie"),
+        Student(id: 4, name: "Daniela"),
+        Student(id: 5, name: "Bob"),
+        
+    ]
     
     // MARK: Computed properties
     var body: some View {
         NavigationStack {
-            List(students, id: \.self) { currentStudent in
-                Text(currentStudent)
+            List(students) { currentStudent in
+                Text(currentStudent.name)
                     .swipeActions {
                         Button("Delete") {
                             withAnimation {
@@ -31,11 +49,11 @@ struct ClassListView: View {
     }
     
     // MARK: Function(s)
-    func delete(providedStudent: String) {
+    func delete(providedStudent: Student) {
         
         // Iterate over the elements of the array and find the student to delete
         for i in 0...students.count - 1 {
-            if students[i] == providedStudent {
+            if students[i].id == providedStudent.id {
                 
                 // Remove the current student
                 students.remove(at: i)
